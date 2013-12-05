7d5ffa30ceba2174798a568e33dbb3cfeb63e8c6
Romain PELISSE <belaran@gmail.com>
2013-12-05 12:17:46 +0100

    make build 'offline' (add -o to mvn call)

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mvn -o clean compile dependency:copy-dependencies -DstripVersion
+mvn clean compile dependency:copy-dependencies -DstripVersion
 if [ ${?} -ne 0 ]; then
   cowsay "Failed !"
 fi
