472bf3a7aad29a546cda99c9abcd1e336eca8051
Romain PELISSE <belaran@gmail.com>
2013-12-05 12:30:35 +0100

    removing link to JDG's libs from run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 readonly NODE_ID=$(basename ${0} | sed -e 's;[A-Za-z\.];;g')
-readonly JDG_REPO=/home/rpelisse/Products/jdg-610-tesco.git/lib
 
-java -cp target/classes:target/dependency/*:${JDG_REPO}/* \
+java -cp target/classes:target/dependency/* \
         -Djava.net.preferIPv4Stack=true -Djgroups.bind_addr=127.0.0.1 \
          "org.infinispan.quickstart.clusteredcache.distribution.Node${NODE_ID}"
